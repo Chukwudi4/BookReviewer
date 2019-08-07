0696e9a68f189ce194a975c34dd0fbe54e933117
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2019-08-07 22:30:09 +0100

    added new api call for book cover

---
+++ config/fetchcover.js
@@ -0,0 +1,18 @@
+
+export async function fetchCover(isbn){
+    console.warn("ssdv")
+    options = {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      };
+
+    url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
+
+    var response = await fetch(url, options)
+    var respJson = await response.json()
+    
+    return respJson.items[0].volumeInfo.imageLinks.thumbnail
+}
